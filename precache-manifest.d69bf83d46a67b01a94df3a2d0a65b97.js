c99151f2eb14684c36c82b389aed5e348f25cf2c
Sahil Soni <SscSPs@gmail.com>
2019-07-28 02:12:06 +0530

    Updates

---
+++ precache-manifest.d69bf83d46a67b01a94df3a2d0a65b97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "542243a6d70cc1f4e39f",
+    "url": "/reactPracticeApp/static/js/runtime~main.e2a2380c.js"
+  },
+  {
+    "revision": "e6689e36044f65cbf367",
+    "url": "/reactPracticeApp/static/js/main.1e601253.chunk.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/reactPracticeApp/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "e6689e36044f65cbf367",
+    "url": "/reactPracticeApp/static/css/main.701be754.chunk.css"
+  },
+  {
+    "revision": "a5f6d71949f82998b95b6833aff48cab",
+    "url": "/reactPracticeApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9a73e0e3bed9cfc1fc9aaa2e86d450b.js"
+  "/reactPracticeApp/precache-manifest.d69bf83d46a67b01a94df3a2d0a65b97.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reactPracticeApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
